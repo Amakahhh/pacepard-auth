907f9f873f3f2b5f35e06f0c7ff2c40ef0418787
damola <thedamolaoladipo@gmail.com>
2025-09-06 16:29:20 +0100

    chore: deployment config

--- src/routes/fallback.tsx
+++ src/routes/fallback.tsx
@@ -1,13 +1,10 @@
 import Notfound from "@/screens/error/not-found";
 import RouteFallback from "@/screens/error/error-ui";
-import UnAAuthorized from "@/screens/error/unauthorized";
+;
 
 
 export const fallbackRoutes = [
-  {
-    path: "/unauthorized",
-    element: <UnAAuthorized />,
-  },
+
   {
     path: "*",
     element: <Notfound />,
